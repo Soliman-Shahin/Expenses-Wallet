b51f536f45c37b0913e459309a3b6c63349211af
Soliman Shahin <sshahin@ntgclarity.com>
2025-08-11 13:26:29 +0300

    update app icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,6 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'sh.expenses.wallet',
   appName: 'Expenses Wallet',
-  bundledWebRuntime: false,
   webDir: 'www',
   server: {
     androidScheme: 'https',
@@ -16,23 +15,8 @@ const config: CapacitorConfig = {
       maxVersions: 2,
     },
     SplashScreen: {
-      launchAutoHide: false,
-      launchShowDuration: 3000,
-    },
-    App: {
-      icon: './src/assets/icon/favicon.png',
-    },
-  },
-  cordova: {
-    preferences: {
-      ScrollEnabled: 'false',
-      'android-minSdkVersion': '19',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000',
+      launchAutoHide: true,
+      launchShowDuration: 1500,
     },
   },
 };
