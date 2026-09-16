7f6409a985e86d3ab2b7d3e34b39a42a41d6c177
Soliman Shahin <shahinsoliman.1994@gmail.com>
2026-09-16 19:14:03 +0300

    test: modernize Angular async test setup

--- src/app/modules/categories/containers/icon-selector/icon-selector.component.spec.ts
+++ src/app/modules/categories/containers/icon-selector/icon-selector.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('IconSelectorComponent', () => {
   let component: IconSelectorComponent;
   let fixture: ComponentFixture<IconSelectorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
     imports: [IconSelectorComponent]
 })
